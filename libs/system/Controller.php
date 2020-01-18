c72846d1f59db9ef5f233d27c64f5ff9bc1c2fde
Moussa SARR <einsarr@gmail.com>
2020-01-18 00:44:44 +0000

    Impléméentation de la Classe controller

--- libs/system/Controller.php
+++ libs/system/Controller.php
@@ -1,9 +1,11 @@
 <?php
 namespace libs\system;
+use libs\system\View;
 class Controller
 {
+    protected $view;
     public function __construct()
     {
-        
+        $this->view = new View();
     }
 }
